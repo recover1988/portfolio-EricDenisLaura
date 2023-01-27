12b434d6bf184015eb867cedb1e8ddd531eeb8ed
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-27 20:29:27 -0300

    skill add

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Skill } from './'
 
 type Props = {}
 
 const Skills = (props: Props) => {
   return (
-    <div>Skills</div>
+    <div className='flex flex-col relative text-center md:text-left xl:flex-row max-w-[2000-x] xl:px-10 min-h-screen justify-center mx-auto items-center xl:space-y-0'>
+
+      <h3 className='titleSection'>Skills</h3>
+
+      <h3 className='absolute top-36 uppercase tracking-[3px] text-color3 text-sm'>Esta son mis habilidades por ahora...</h3>
+
+
+
+      <div className='grid grid-cols-4 gap-5'>
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
+    </div>
   )
 }
 

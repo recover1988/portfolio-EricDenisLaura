3699cbea40e3935cfb9a8f9671af93554f61d502
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-26 23:52:01 -0300

    add foto

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -4,15 +4,28 @@ import React from 'react'
 type Props = {}
 
 const BackgroundCircles = (props: Props) => {
-  return (
-    <div className='relative flex justify-center items-center'>
-        <div className='absolute border-2 border-color5 rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
-        <div className='absolute border border-color2 rounded-full h-[300px] w-[300px] mt-52 '/>
-        <div className='absolute border border-color2 rounded-full h-[500px] w-[500px] mt-52 '/>
-        <div className='absolute border-4 border-color5 opacity-20 rounded-full h-[650px] w-[650px] mt-52 animate-pulse'/>
-        <div className='absolute border border-color2 rounded-full h-[800px] w-[800px] mt-52 animate-pulse opacity-0' />
-    </div>
-  )
+    return (
+        <motion.div
+            initial={{
+                opacity: 0,
+            }}
+            animate={{
+                scale: [1, 2, 2, 3, 1],
+                opacity: [0.1, 0.2, 0.8, 0.1, 1.0],
+                borderRadius: ['20%', '20%', '50%', '80%', '20%',],
+            }}
+            transition={{
+                duration: 2.5,
+            }}
+            className='relative flex justify-center items-center'
+        >
+            <div className='absolute border border-color5 rounded-full h-[400px] w-[400px] mt-52 animate-ping' />
+            {/* <div className='rounded-full border border-color2  h-[300px] w-[300px] absolute mt-52 ' /> */}
+            {/* <div className='rounded-full border border-color2  h-[500px] w-[500px] absolute mt-52 ' /> */}
+            <div className='rounded-full border-2 border-color5 opacity-0  h-[550px] w-[550px] absolute mt-52 animate-pulse' />
+            <div className='rounded-full border-4 border-color5  h-[800px] w-[800px] absolute mt-52 animate-pulse opacity-40' />
+        </motion.div>
+    )
 }
 
 export default BackgroundCircles
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
-import {BackgroundCircles} from './'
+import { BackgroundCircles } from './'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
+import miImagen from '../public/fotoperfil.jpg'
 
 type Props = {}
 
@@ -9,10 +11,9 @@ const Hero = (props: Props) => {
 
     const [text, count] = useTypewriter({
         words: [
-            'Hola, Mi Nombre Es Eric Denis Laura Isnado.',
-            'const meGusta = "hace calor" ? "nadar" : "leer novelas" ;',
-            '<PeroMeGustaMasCodear typescript javascript />',
-            'FullStackDeveloper.tsx'
+            'Hola, Yo soy Eric Denis Laura Isnado.',
+            'Me-gusta-tomar-té.tsx" ;',
+            '<PeroMeGustaMasCodear />',
         ],
         loop: true,
         delaySpeed: 2000,
@@ -21,10 +22,32 @@ const Hero = (props: Props) => {
     return (
         <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
             <BackgroundCircles />
-            <h1>
-                <span>{text}</span>
-                <Cursor cursorColor='#ff0062' />
-            </h1>
+            <Image
+                src={miImagen}
+                alt='Mi imagen'
+                className='relative rounded-full h-40 w-40 object-cover mx-auto'
+            />
+            <div>
+                <h2
+                    className='text-sm uppercase text-color4 pb-2 tracking-[15px]'
+                >
+                    Full Stack Developer
+                </h2>
+                <h1>
+                    <span
+                        className='text-5xl lg:text-6xl font-semibold px-10'
+                    >
+                        {text}
+                    </span>
+                    <Cursor cursorColor='#ff0062' />
+                </h1>
+                <div>
+                    <button>About</button>
+                    <button>Experience</button>
+                    <button>Skills</button>
+                    <button>Projects</button>
+                </div>
+            </div>
         </div>
     )
 }

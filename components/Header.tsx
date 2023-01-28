962ce1dbce657b9cd4bfa860e4fe3fed0b3c2322
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-28 20:27:11 -0300

    fix bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
 import { motion } from "framer-motion"
+import Link from 'next/link';
 type Props = {}
 
 const Header = (props: Props) => {
@@ -43,30 +44,33 @@ const Header = (props: Props) => {
           target='_blank'
         />
       </motion.div>
-      <motion.div
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 1.5,
-        }}
-        className='flex flex-row items-center  cursor-pointer'
-      >
-        <SocialIcon
-          className='cursor-pointer'
-          network='email'
-          fgColor='#b3b4bd'
-          bgColor='transparent'
-        />
-        <p className='uppercase hidden md:inline-flex text-sm text-color3'>Contacta Me!</p>
-      </motion.div>
+      <Link href='#contactme' passHref>
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+          className='flex flex-row items-center  cursor-pointer'
+        >
+          <SocialIcon
+            className='cursor-pointer'
+            network='email'
+            fgColor='#b3b4bd'
+            bgColor='transparent'
+          />
+          <p className='uppercase hidden md:inline-flex text-sm text-color3'>Contacta Me!</p>
+        </motion.div>
+      </Link>
+
     </header>
   )
 }

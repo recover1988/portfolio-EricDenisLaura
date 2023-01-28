4a420d2c7bf2a6f27ff4623c9c3da00c5686669f
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-28 11:55:34 -0300

    create the h-screen for contact

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -4,7 +4,10 @@ type Props = {}
 
 const ContactMe = (props: Props) => {
   return (
-    <div>ContactMe</div>
+    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      <h3 className='titleSection'>Contact</h3>
+
+    </div>
   )
 }
 

41f2b240a9042d2470b98d7eed931b8cf712e5af
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-29 16:20:59 -0300

    create base de datos

--- components/CardProject.tsx
+++ components/CardProject.tsx
@@ -1,5 +1,7 @@
 import { motion } from 'framer-motion'
 import React from 'react'
+import ReactPlayer from 'react-player/lazy'
+import { SocialIcon } from 'react-social-icons'
 
 type Props = {
   id: number,
@@ -10,27 +12,34 @@ type Props = {
   repository: string,
 }
 
-const CardProject = ({ }: Props) => {
+const CardProject = ({id,name,img,video,description,repository }: Props) => {
   return (
-    <div key={proj} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
-      <motion.img
+    <div key={id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+      <motion.div
         initial={{
           y: -300,
           opacity: 0
         }}
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 0 }}
-        // viewport={{ once: true }}
-        src="https://st2.depositphotos.com/3591429/5246/i/450/depositphotos_52462511-stock-photo-catch-a-big-idea.jpg"
-        alt="comp"
-
-      />
+        viewport={{ once: true }}
+      >
+        <ReactPlayer url='https://youtu.be/ZgaNCsbAu7A' />
+      </motion.div>
       <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
         <h4 className='text-2xl font-semibold text-center'>
-          <span className='underline underline-offset-4 decoration-4 decoration-color5/50'>Aplicacion No{index + 1}:</span>
-          Proyecto Individual
+          <span className='underline underline-offset-4 decoration-4 decoration-color5/50'>Aplicacion No{id}:</span>
+          {name}
         </h4>
-        <p className='text-lg text-center md:text-left'>Api Dogs: es una aplicacion para buscar perros atravez de diferentes filtros, como la raza, tamaño, espectativa de vida, etc y combinar los filtros. Tambien tiene una base de datos con la cual atravez de un formulario puedes subir una raza de perros nueva y guardarla.</p>
+        <p className='text-lg text-center md:text-left'>
+      {description}
+          </p>
+          <SocialIcon
+          url={repository}
+          fgColor='#004CFF'
+          bgColor='transparent'
+          target='_blank'
+        />
       </div>
     </div>
   )
---
+++ database/DBprojects.ts
@@ -0,0 +1,33 @@
+export interface IProjects {
+    id: number,
+    name: string,
+    img?: string,
+    video: string,
+    description: string,
+    repository: string,
+}
+
+
+export const projects: IProjects[] = [
+    {
+        id: 1,
+        name: 'Pawsitive',
+        video: 'https://youtu.be/aog7KP-JIuE',
+        description: 'Es una aplicacion para la adopción de mascotas y la venta de productos en la Petshop, esta desarrollada para poder mejorar la adopcion de mascotas entres los usuarios mediante formularios que facilitan obtener la información necesaria para llevar a cabo una adopcion exitosa. Tambien cuenta con una petshop de productos los cuales se puede poner en favorito o al carrito y realizar compras. La aplicacion esta desarrollada en NextJS y TypeScritpt, tambien usa Auth0, Stripe, TailwindCSS, Tailwind, Prisma, PostgreSQL, entre otras. Tiene un Dashboard donde se muestras diferentes registros en tablas y se puede ver mediante graficos en ChartJS diferentes estadisticas.',
+        repository: 'https://pawsitive-app.vercel.app/',
+    },
+    {
+        id: 2,
+        name: 'Dogs APP',
+        video: 'https://youtu.be/BFc267mgEpQ',
+        description: 'Esta aplicacion esta desarrollada con el stack PERN (PostgreSQL, Express, React, NodeJS), es una aplicacion donde se aplican diferentes fundamentos como obtener datos de un Endpoint, manipular datos y mostrarlos en pantalla, aplicar filtros de busqueda, creacion de datos con verificacion atravez de formularios, y mas. Esta realizada en React con Javascript y tambien se uso Redux, Express, CSS Modules, PostgreSQL, Sequelize, NodeJS.',
+        repository: 'https://github.com/recover1988/PI-SoyHenry-Dogs',
+    },
+    {
+        id: 3,
+        name: 'Teslo Shop',
+        video: 'https://youtu.be/ZgaNCsbAu7A',
+        description: 'Es una Tienda clon de Tesla Shop, fue desarrollada en NextJS con TypeScript. En esta aplicacion se usa Cookies para hacer la autentificacion por NextAuth, los pagos se realizan con Paypal, se uso MongoDb como base de datos para guardar los productos, usuarios y regitro de compras. Formularios para crear productos con Cloudinary para las imagenes. Cuenta con dashboard para ver los productos, usaurios y registros de venta. ',
+        repository: 'https://pawsitive-app.vercel.app/',
+    },
+]
